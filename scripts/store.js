da18c5bb6df072bf3369963a887b2a53d92e2d67
Badri-narayan <badritulsiram@gmail.com>
2019-08-28 15:46:10 -0700

    brooke it

---
+++ scripts/store.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const store = (function () {
+  const bookmarks = [];
+​
+  const setError = function(err) {
+    this.error = err;
+  }
+​
+  const clearError = function() {
+    this.error = null;
+  }
+    
+  const delBookmark = function(id) {
+    this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id)
+  }
+​
+  const _decorateBookmark = function(bookmark){
+    return Object.assign(bookmark, { expanded: false });
+  }
+​
+  const addBookmark = function(bookmark) {
+    this.bookmarks.push(_decorateBookmark(bookmark));
+  }
+​
+  return {
+    adding: false,
+    bookmarks,
+    minRating: 0,
+    error: null,
+​
+    setError,
+    clearError,
+    delBookmark,
+    addBookmark,
+  };
+})();
